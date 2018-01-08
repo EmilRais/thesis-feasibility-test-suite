b269d7ea7356a90f1afd9636844fa4b3e2277a86
Emil Rais <emilrais@outlook.com>
2018-01-08 23:47:24 +0100

    Now preloading all images so as to not seem to take an unnerving amount of time to run with no images present

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+function load-image {
+   image=$1
+   if [[ "$(docker images -q $image 2> /dev/null)" == "" ]]; then
+       docker pull $image
+   fi
+}
+
 function wait {
     container=$1
     message=$2
@@ -10,6 +17,13 @@ function wait {
     do sleep 0.1;
     done
 }
+
+load-image mongo
+load-image emilrais/thesis-loom
+load-image emilrais/thesis-alpha-api-manual
+load-image emilrais/thesis-alpha-api-generated
+load-image emilrais/thesis-beta-api-manual
+load-image emilrais/thesis-beta-api-generated
 
 npm run build
 docker network create restnet > /dev/null 2>&1
